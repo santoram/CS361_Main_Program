e248dc767216d64a094fb4b98aa94ab091f2cd0a
Mike Santora <santoram@oregonstate.edu>
2025-08-09 14:04:52 -0400

    ``

--- main-react/src/components/exerciseRecommend.jsx
+++ main-react/src/components/exerciseRecommend.jsx
@@ -1,12 +1,13 @@
 function RecommendedExercises({target}) {
     const exercises = target.Exercises;
     console.log(target)
- 
+    // console.log(exercises)
+    
 
 
     return (   
         <div>
-            <h4>Recommended Exercises</h4>
+            <h1>Recommended Exercises</h1>
             {exercises.map((exercise, index) => (<p key={index}>{exercise}</p> ))}
         </div>
     );
